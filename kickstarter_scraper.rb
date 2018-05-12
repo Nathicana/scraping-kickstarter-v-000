530153be7c6a0233dbb10bed8c5cd0da5af8de40
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-05-12 23:04:18 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -21,9 +21,9 @@ kickstarter.css("li.project.grid_4").each do |project|
   #convert project title to symbol because symbols make better hash keys than strings
   projects[title.to_sym] = {
 
-  :image_link => project.css("div.project-thumbnail a img").attributes("src").value
-  :description => project.css("p.bbcard_blurb").text
-  :location => project.css("span.location-name").text
+  :image_link => project.css("div.project-thumbnail a img").attributes("src").value,
+  :description => project.css("p.bbcard_blurb").text,
+  :location => project.css("span.location-name").text,
   :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
 }
 end
