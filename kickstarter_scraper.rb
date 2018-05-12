3a5b3e5d4d64e097a8ee63f910555e75b7557708
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-05-12 22:29:09 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,6 +11,7 @@ def create_project_hash
 # image link: project.css("div.project-thumbnail a img").attributes("src").value
 #description: project.css("p.bbcard_blurb").text
 #project.css("span.location-name").text
+#project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
 binding.pry
 end
 
